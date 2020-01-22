5ceca666bc5d3816c92015abeaa5432150e8f3e3
Eric Ma <ericmajinglong@gmail.com>
2020-01-22 13:04:06 -0500

    updated setup.py to ship with license and weights

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import find_packages, setup
 
-setup(name="jax_unirep", version="0.1", packages=find_packages())
+setup(
+    name="jax_unirep",
+    version="0.1",
+    packages=find_packages(),
+    package_data={
+        '': ["LICENSE", "weights/*"],
+    },
+    include_package_data=True,
+)
