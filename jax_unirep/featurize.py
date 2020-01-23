9ce980554038f21092ae0130ca4f2669514e4e87
ElArkk <arkadij.kummer@gmail.com>
2020-01-23 12:57:38 +0100

    Better docstrings

--- jax_unirep/featurize.py
+++ jax_unirep/featurize.py
@@ -6,30 +6,17 @@ from .layers import mlstm1900
 from .utils import batch_sequences, get_embeddings, load_params_1900
 
 
-def get_reps_samelength(
+def rep_same_lengths(
     seqs: List[str],
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    This function generates representations of protein sequences using the 
-    1900 hidden-unit mLSTM model with pre-trained weights from the UniRep
-    paper (https://github.com/churchlab/UniRep).
-
-    All input sequences need to be of the same length!
-
-    Each element of the output tuple is a np.array
-    of shape (n_sequences, 1900).
-
-    You should not use this function if you want to do further JAX-based computations
-    on the output vectors! In that case, the `DeviceArray` futures returned by `mlstm1900`
-    should be passed directly into the next step instead of converting them to np.arrays.
-    The conversion to np.arrays is done here to force python to wait with returning the values
-    until the computation is actually completed.
-
+    This function generates representations of protein sequences that have the same length,
+    by passing them through the UniRep mLSTM.
 
     :param seqs: A list of same length sequences as strings.
         If passing only a single sequence, it also needs to be passed inside a list.
     :returns: A tuple of np.arrays containing the reps.
-        Each np.array has shape (n_sequences, 1900).
+        Each `np.array` has shape (n_sequences, 1900).
     """
 
     params = load_params_1900()
@@ -41,10 +28,19 @@ def get_reps_samelength(
     return np.array(h_final), np.array(c_final), np.array(h_avg)
 
 
-def get_reps_arbitrarylength(
+def rep_arbitrary_lengths(
     seqs: List[str],
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+"""
+This function generates representations of protein sequences of arbitrary length,
+by batching together all sequences of the same length and passing them through
+the mLSTM. Original order of sequences is restored in the final output.
 
+:param seqs: A list of sequences as strings.
+    If passing only a single sequence, it also needs to be passed inside a list.
+:returns: A 3-tuple of `np.array`s containing the reps.
+    Each `np.array` has shape (n_sequences, 1900).
+"""
     order = batch_sequences(seqs)
     # TODO: Find a better way to do this, without code triplication
     hf_list, cf_list, ha_list = [], [], []
@@ -74,7 +70,29 @@ def get_reps_arbitrarylength(
 
 
 def get_reps(seqs: List[str]) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    This function generates representations of protein sequences using the 
+    1900 hidden-unit mLSTM model with pre-trained weights from the UniRep
+    paper (https://github.com/churchlab/UniRep).
 
+    Each element of the output 3-tuple is a `np.array`
+    of shape (n_input_sequences, 1900):
+    - `h_final`: Final hidden state of the mLSTM
+    - `c_final`: Final cell state of the mLSTM
+    - `h_avg`: Average hidden state of the mLSTM over the whole sequence.
+
+    You should not use this function if you want to do further JAX-based computations
+    on the output vectors! In that case, the `DeviceArray` futures returned by `mlstm1900`
+    should be passed directly into the next step instead of converting them to `np.array`s.
+    The conversion to `np.array`s is done here to force python to wait with returning the values
+    until the computation is actually completed.
+
+
+    :param seqs: A list of sequences as strings.
+        If passing only a single sequence, it also needs to be passed inside a list.
+    :returns: A 3-tuple of `np.array`s containing the reps.
+        Each `np.array` has shape (n_sequences, 1900).
+    """
     # Make sure list is not empty
     if len(seqs) == 0:
         raise SequenceLengthsError("Cannot pass in empty list of sequences.")
@@ -83,8 +101,8 @@ def get_reps(seqs: List[str]) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     # 1. All sequences in the list have the same length
     # 2. There are sequences of different lengths in the list
     if len(set([len(s) for s in seqs])) == 1:
-        h_final, c_final, h_avg = get_reps_samelength(seqs)
+        h_final, c_final, h_avg = rep_same_lengths(seqs)
         return h_final, c_final, h_avg
     else:
-        h_final, c_final, h_avg = get_reps_arbitrarylength(seqs)
+        h_final, c_final, h_avg = rep_arbitrary_lengths(seqs)
         return h_final, c_final, h_avg
