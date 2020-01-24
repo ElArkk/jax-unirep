db2ff1fdba391526bb6cb43577deeece2b2b073d
Eric Ma <ericmajinglong@gmail.com>
2020-01-24 05:49:49 -0500

    added dependencies to setup script

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ setup(
     name="jax_unirep",
     version="0.1",
     packages=["jax_unirep"],
-#     data_files=[("weights", ["weights/1900_weights/*.npy"])],
     package_data={
         'jax_unirep': ["weights/1900_weights/*.npy"],
     },
+    install_requires=[
+        'jax',
+        'jaxlib',
+        'numpy',
+    ]
     include_package_data=True,
 )
