b69ce4702e2d7547b7327e26a4dbedaf53f4f95f
ElArkk <arkadij.kummer@gmail.com>
2020-01-30 18:07:40 +0100

    moved pre-trained weights to specifically named folder

--- jax_unirep/utils.py
+++ jax_unirep/utils.py
@@ -55,8 +55,8 @@ def aa_seq_to_int(s):
     return [24] + [aa_to_int[a] for a in s] + [25]
 
 
-def load_embedding_1900():
-    return np.load(weights_1900_dir/ "embed_matrix:0.npy")
+def load_embedding_1900(name: str = 'UniRef50'):
+    return np.load(weights_1900_dir/name/"embed_matrix:0.npy")
 
 
 def get_embedding(sequence: str, embeddings: np.ndarray) -> np.ndarray:
@@ -96,37 +96,37 @@ Sequence length: number of sequences information in the dictionary below.
     return onp.stack(seq_embeddings, axis=0)
 
 
-def load_params_1900() -> dict:
+def load_params_1900(name: str = 'UniRef50') -> dict:
 
     params = dict()
     params["gh"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_gh:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_gh:0.npy"
     )
     params["gmh"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_gmh:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_gmh:0.npy"
     )
     params["gmx"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_gmx:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_gmx:0.npy"
     )
     params["gx"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_gx:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_gx:0.npy"
     )
 
     params["wh"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_wh:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_wh:0.npy"
     )
     params["wmh"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_wmh:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_wmh:0.npy"
     )
     params["wmx"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_wmx:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_wmx:0.npy"
     )
     params["wx"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_wx:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_wx:0.npy"
     )
 
     params["b"] = np.load(
-        weights_1900_dir/"rnn_mlstm_mlstm_b:0.npy"
+        weights_1900_dir/name/"rnn_mlstm_mlstm_b:0.npy"
     )
 
     return params
