43986981b3543a59fef4a2a01ac33f9394df9ac1
ElArkk <arkadij.kummer@gmail.com>
2020-01-31 17:27:48 +0100

    Changed init back to normal dist

--- jax_unirep/params.py
+++ jax_unirep/params.py
@@ -4,26 +4,26 @@ from jax.random import PRNGKey, normal, split
 key = PRNGKey(42)
 
 
-def add_dense_params(params, name, input_dim, output_dim, init=glorot_normal):
+def add_dense_params(params, name, input_dim, output_dim):
     params[name] = dict()
-    params[name]["w"] = init(split(key)[0], (input_dim, output_dim)) * 0.01
-    params[name]["b"] = init(split(key)[0], (output_dim,)) * 0.01
+    params[name]["w"] = normal(split(key)[0], (input_dim, output_dim)) * 0.01
+    params[name]["b"] = normal(split(key)[0], (output_dim,)) * 0.01
     return params
 
 
 def add_mlstm1900_params(
-    params, name, input_dim, output_dim, init=glorot_normal
+    params, name, input_dim, output_dim
 ):
     params[name] = dict()
-    params[name]["wmx"] = init(split(key)[0], (input_dim, output_dim))
-    params[name]["wmh"] = init(split(key)[0], (output_dim, output_dim))
-    params[name]["wx"] = init(split(key)[0], (input_dim, output_dim * 4))
-    params[name]["wh"] = init(split(key)[0], (output_dim, output_dim * 4))
+    params[name]["wmx"] = normal(split(key)[0], (input_dim, output_dim))
+    params[name]["wmh"] = normal(split(key)[0], (output_dim, output_dim))
+    params[name]["wx"] = normal(split(key)[0], (input_dim, output_dim * 4))
+    params[name]["wh"] = normal(split(key)[0], (output_dim, output_dim * 4))
 
-    params[name]["gmx"] = init(split(key)[0], (output_dim,))
-    params[name]["gmh"] = init(split(key)[0], (output_dim,))
-    params[name]["gx"] = init(split(key)[0], (output_dim * 4,))
-    params[name]["gh"] = init(split(key)[0], (output_dim * 4,))
+    params[name]["gmx"] = normal(split(key)[0], (output_dim,))
+    params[name]["gmh"] = normal(split(key)[0], (output_dim,))
+    params[name]["gx"] = normal(split(key)[0], (output_dim * 4,))
+    params[name]["gh"] = normal(split(key)[0], (output_dim * 4,))
 
-    params[name]["b"] = init(split(key)[0], (output_dim * 4,))
+    params[name]["b"] = normal(split(key)[0], (output_dim * 4,))
     return params
