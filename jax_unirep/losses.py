4e397a25226016880cca37698223d6960467122d
Eric Ma <ericmajinglong@gmail.com>
2020-01-31 16:56:14 -0500

    Added tolerance to losses

--- jax_unirep/losses.py
+++ jax_unirep/losses.py
@@ -12,16 +12,17 @@ def _cross_entropy_loss(y, y_hat):
     return np.mean(xent)
 
 
-def _neg_cross_entropy_loss(y, y_hat, tol = 1E-10):
+def _neg_cross_entropy_loss(y, y_hat, tol=1e-10):
     """
     Also corresponds to the log likelihood of the Bernoulli
     distribution.
     Intended to be used inside of another function that differentiates w.r.t.
     parameters.
     """
-    xent = -(y * np.log(y_hat + tol) + (1 - y) * np.log((1 - y_hat) + tol))
+    xent = -(
+        y * np.log(np.maximum(tol, y_hat)) + (1 - y) * np.log(np.maximum(tol, 1 - y_hat))
+    )
     return np.mean(xent)
-
 
 def _mse_loss(y, y_hat):
     """
