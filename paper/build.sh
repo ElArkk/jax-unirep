565e091207a8fd0985bbaddf0451562134236f46
Eric Ma <ericmajinglong@gmail.com>
2020-02-04 09:40:13 -0500

    Added additional formatting

--- paper/build.sh
+++ paper/build.sh
@@ -1,3 +1,12 @@
-pandoc paper.md -o index.html
+pandoc paper.md -o index.html \
+	--css style.css \
+	--mathjax \
+	--filter pandoc-fignos \
+	--filter pandoc-tablenos \
+	--filter pandoc-citeproc \
+	--bibliography references.bib \
+	-s
 
-pandoc paper.md -o paper.pdf
+pandoc paper.md -o paper.pdf \
+	--filter pandoc-citeproc \
+	--bibliography references.bib \
