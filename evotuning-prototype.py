371bbce5d4a84bdea5740ddc307b3258038d0bac
Eric Ma <ericmajinglong@gmail.com>
2020-02-07 10:55:22 -0500

    Convert evotuning prototype to python script

--- evotuning-prototype.py
+++ evotuning-prototype.py
@@ -1,4 +1,4 @@
-from jax_unirep.evotuning import evotune
+from jax_unirep import evotune
 from jax_unirep.utils import load_params_1900
 from jax_unirep.params import add_dense_params
 
@@ -10,5 +10,14 @@ params["mlstm1900"] = load_params_1900()
 params = add_dense_params(params, "dense", 1900, 25)
 
 params = evotune(params, sequences, n=10)
+
+# from jax_unirep.evotuning import evotuning_pairs
+# from jax_unirep.utils import letter_seq
+
+# x, y = evotuning_pairs("HASTA")
+# seq = letter_seq(y)
+# print(seq)
+# Convert y back to string
+
 
 # Diagnosis - from integers back to sequences.
