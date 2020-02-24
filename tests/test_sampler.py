95650e1678a9f5399542ab59fd91acbdcc51355e
Arkadij Kummer <43340666+ElArkk@users.noreply.github.com>
2020-02-24 17:12:05 +0100

    Sequence Sampling (#22)
    * Added function which checks candidate mutant vs best mutant, plus test
    
    * check_acceptance return annotation and more docstrings
    
    * changed ``test_acceptance`` to ``is_accepted``
    
    * Sequence sampler (#21)
    
    * Added multipledispatch to environment
    
    * Added a function to propose new strings
    
    * Check that new proposed sequence is of same length as original
    
    * refactored ``propose`` function out to ``sampler.py``
    
    - also moved test
    
    Co-authored-by: Eric Ma <ericmjl@users.noreply.github.com>

---
+++ tests/test_sampler.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+from hypothesis import given, settings
+from hypothesis import strategies as st
+
+from jax_unirep.sampler import is_accepted, propose
+from jax_unirep.utils import proposal_valid_letters
+
+
+@given(st.data())
+# @settings(deadline=None, max_examples=100)
+def test_is_accepted(data):
+    best = data.draw(
+        st.floats(
+            min_value=0, max_value=1e2, allow_nan=False, allow_infinity=False
+        )
+    )
+    candidate = data.draw(st.floats(allow_nan=False, allow_infinity=False))
+    accept = is_accepted(best=best, candidate=candidate)
+    assert isinstance(accept, bool)
+
+
+@given(seq=st.text(alphabet=proposal_valid_letters, min_size=1))
+def test_propose_string(seq):
+    new_seq = propose(seq)
+    assert new_seq != seq
+    assert len(new_seq) == len(seq)
+
+    # Check that only one position is different
+    different_positions = []
+    for i, (l1, l2) in enumerate(zip(seq, new_seq)):
+        if l1 != l2:
+            different_positions.append(i)
+    assert len(different_positions) == 1
