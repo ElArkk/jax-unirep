5bbe198ccaac8a6ed3cc53acd40015de12dd6005
ElArkk <arkadij.kummer@gmail.com>
2020-05-01 16:11:39 +0200

    docstrings

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -51,17 +51,29 @@ def test_get_batch_len():
 
 
 def test_load_dense_1900():
+    """
+    Make sure that parameters to be passed to 
+    the dense layer of the evotuning stax model have the right shapes.
+    """
     dense = load_dense_1900()
     assert dense[0].shape == (1900, 25)
     assert dense[1].shape == (25,)
 
 
 def test_load_params_1900():
+    """
+    Make sure that parameters to be passed to 
+    the mlstm1900 have the right shapes.
+    """
     params = load_params_1900()
     validate_mLSTM1900_params(params)
 
 
 def test_load_embedding_1900():
+    """
+    Make sure that the inital 10 dimensional aa embedding vectors 
+    have the right shapes.
+    """
     emb = load_embedding_1900()
     assert emb.shape == (26, 10)
 
@@ -75,11 +87,19 @@ def validate_params(params):
 
 
 def test_load_params():
+    """
+    Make sure that all parameters needed for the evotuning stax model
+    get loaded with the correct shapes.
+    """
     params = load_params()
     validate_params(params)
 
 
 def test_dump_params():
+    """
+    Make sure that the parameter dumping function used in evotuning
+    conserves all parameter shapes correctly.
+    """
     params = load_params()
     dump_params(params, "tmp")
     dumped_params = load_params("tmp/iter_0")
