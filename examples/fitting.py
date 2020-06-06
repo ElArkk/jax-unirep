9b93afc58b2bcf0e2db6706a1ee13423830a7cbc
Arkadij Kummer <43340666+ElArkk@users.noreply.github.com>
2020-06-06 12:15:03 +0200

    Random batching (#48)
    * upload coverage report to codecov
    
    * install codecov
    
    * random batching function with padding, plus tests
    
    * remove work from different branch
    
    * make format
    
    * random batch fit function draft
    
    * Unify both batching methods in one fit function
    
    * back to one universal fit function
    
    - random and length batching handled
    - iterations handled in single loop
    
    * small fixes
    
    * remove old code
    
    * docstrings, remove old code
    
    * more testing
    
    * init file now present in source
    
    * make format
    
    * fix local variables in batching func
    
    * Update docstring
    
    Co-authored-by: Eric Ma <ericmjl@users.noreply.github.com>
    
    * Update docstring
    
    Co-authored-by: Eric Ma <ericmjl@users.noreply.github.com>
    
    * fix evotuning example
    
    * Further code simplifications
    
    * Change file name to enzymes.fa
    
    * Added conditional to handle holdout seqs
    
    * Fix objective
    
    * Changed all seqs to be Iterable types
    
    * Fixed all tests
    
    * Blessed is the developer who deletes code
    
    * Update readme
    
    Co-authored-by: Eric Ma <ericmjl@users.noreply.github.com>
    Co-authored-by: Eric Ma <ericmajinglong@gmail.com>

---
+++ examples/fitting.py
@@ -0,0 +1,45 @@
+"""Fitting on a bunch of sequences."""
+import logging
+from random import shuffle
+
+from Bio import SeqIO
+from pyprojroot import here
+
+from jax_unirep import fit
+from jax_unirep.utils import dump_params
+
+
+seqs = []
+with open(here() / "examples/enzymes.fa", "r+") as f:
+    for record in SeqIO.parse(f, "fasta"):
+        seqs.append(str(record.seq))
+
+
+seqs = seqs[:50]
+shuffle(seqs)
+break_point = int(len(seqs) * 0.7)
+sequences = seqs[0:break_point]
+holdout_sequences = seqs[break_point:]
+
+
+logger = logging.getLogger("fitting.py")
+logger.setLevel(logging.DEBUG)
+logger.info(f"There are {len(sequences)} sequences.")
+
+N_EPOCHS = 20
+LEARN_RATE = 1e-5
+PROJECT_NAME = "temp"
+
+evotuned_params = fit(
+    params=None,
+    sequences=sequences,
+    n_epochs=N_EPOCHS,
+    step_size=LEARN_RATE,
+    holdout_seqs=holdout_sequences,
+    batch_method="random",
+    proj_name=PROJECT_NAME,
+    steps_per_print=None,
+)
+
+dump_params(evotuned_params, PROJECT_NAME)
+print("Evotuning done! Find output weights in", PROJECT_NAME)
