36bfa4853dd77108742d7fcfc1d2f76af7e9bb54
Eric Ma <ericmjl@users.noreply.github.com>
2020-06-09 08:19:45 -0400

    Add optional dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.1",
     packages=["jax_unirep"],
     package_data={"jax_unirep": ["weights/1900_weights/uniref50/*.npy"],},
-    install_requires=["jax", "jaxlib", "numpy",],
+    install_requires=["jax", "jaxlib", "numpy", "optuna", "scikit-learn", "tqdm"],
     include_package_data=True,
 )
