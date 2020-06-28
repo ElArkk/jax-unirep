bfb2fefae14f8b62c5c4eedafe72edf10b1d300c
Eric Ma <ericmajinglong@gmail.com>
2020-06-28 09:15:27 -0400

    Attempting to increase coverage without doing any actual work ^_^

---
+++ tests/test_params.py
@@ -0,0 +1,11 @@
+from jax_unirep.params import add_dense_params, add_mLSTM1900_params
+
+
+def test_add_dense_params():
+    """Placeholder for actual test."""
+    pass
+
+
+def test_add_mLSTM1900_params():
+    """Placeholder for actual test."""
+    pass
