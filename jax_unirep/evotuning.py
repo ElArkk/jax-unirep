50fe1700836ae62ea46224aef65bc92c035fef69
konstin <konstin@mailbox.org>
2020-08-29 21:32:57 +0200

    Reformat jax_unirep/evotuning.py

--- jax_unirep/evotuning.py
+++ jax_unirep/evotuning.py
@@ -40,7 +40,9 @@ def setup_evotuning_log():
     logger.setLevel(logging.INFO)
     fh = logging.FileHandler("evotuning.log")
     fh.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s :: %(levelname)s :: %(message)s"
+    )
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
