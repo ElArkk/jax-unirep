5acc431ec31694ab37d2e6ee602f07897820bd33
Eric Ma <ericmajinglong@gmail.com>
2020-09-18 08:47:21 -0400

    Removed unnecessary indirection

--- jax_unirep/utils.py
+++ jax_unirep/utils.py
@@ -335,10 +335,9 @@ def get_batching_func(seq_batch, batch_size: int = 25) -> Callable:
     """
 
     def batching_func():
-        seqs = seq_batch
-        if len(seqs) > batch_size:
-            seqs = sample(seqs, batch_size)
-        xs, ys = input_output_pairs(seqs)
+        if len(seq_batch) > batch_size:
+            seq_batch = sample(seq_batch, batch_size)
+        xs, ys = input_output_pairs(seq_batch)
         return xs, ys
 
     return batching_func
