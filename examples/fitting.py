a6360761fde9cdad7be762d271a368e65a35e0f8
Eric Ma <ericmajinglong@gmail.com>
2020-10-02 17:53:42 -0400

    Fixed examples

--- examples/fitting.py
+++ examples/fitting.py
@@ -5,9 +5,10 @@ from Bio import SeqIO
 from pyprojroot import here
 
 from jax_unirep import fit
-from jax_unirep.utils import dump_params, load_random_evotuning_params
+from jax_unirep.utils import dump_params
+from jax.random import PRNGKey
 
-"""Fitting on a bunch of sequences."""
+"""Fitting mLSTM weights based on randomly initialized weights."""
 
 
 seqs = []
@@ -31,19 +32,19 @@ N_EPOCHS = 20
 LEARN_RATE = 1e-5
 PROJECT_NAME = "temp"
 
-params = load_random_evotuning_params()
-
 evotuned_params = fit(
-    params=params,
+    mlstm_size=256,
+    rng=PRNGKey(42),
+    params=None,
     sequences=sequences,
     n_epochs=N_EPOCHS,
     step_size=LEARN_RATE,
     holdout_seqs=holdout_sequences,
+    batch_size=2000,
     batch_method="random",
     proj_name=PROJECT_NAME,
     epochs_per_print=1,
-    backend="cpu",  # default is "cpu"
+    backend="cpu",  # default is "cpu", can be "gpu" if you have JAX-GPU installed.
 )
-
 dump_params(evotuned_params, PROJECT_NAME)
 print("Evotuning done! Find output weights in", PROJECT_NAME)
