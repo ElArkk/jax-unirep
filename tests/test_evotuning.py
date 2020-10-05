7fb549217e223c48713c78fe1bb0889bb40f3387
Eric Ma <ericmajinglong@gmail.com>
2020-10-05 16:55:20 -0400

    Deleted unused tests

--- tests/test_evotuning.py
+++ tests/test_evotuning.py
@@ -16,46 +16,24 @@ from .test_layers import validate_mLSTM_params
 
 @pytest.fixture
 def params():
+    """Return randomly initialized params."""
     model_layers = evotuning_layers(mlstm_size=64)
     init_fun, _ = stax.serial(*model_layers)
     _, parameters = init_fun(PRNGKey(0), (-1, 10))
     return parameters
 
 
-# @pytest.mark.skip(reason="Maybe deprecate?")
 def test_evotune():
-    """
-    Simple execution test for evotune.
-    """
+    """Simple execution test for evotune."""
     seqs = ["MTN", "BDD"] * 5
     n_epochs_config = {"high": 1}
     fit_func = partial(fit, mlstm_size=256, rng=PRNGKey(0))
-    _, params_new = evotune(
+    _, _ = evotune(
         sequences=seqs,
         fit_func=fit_func,
         n_trials=1,
         n_epochs_config=n_epochs_config,
     )
-
-
-@pytest.mark.skip(reason="Not needed.")
-def test_predict(params):
-    """
-    Unit test for ``jax_unirep.evotuning.predict``.
-
-    We test that the shape of the output of ``predict``
-    is identical to the shape of the ys to predict.
-
-    We also test that the evotune `predict` function gives us bounded values
-    that are between 0 and 1.
-    """
-    sequences = ["ASDFGHJKL", "ASDYGHTKW"]
-    xs, ys = input_output_pairs(sequences)
-    res = vmap(partial(predict, params))(xs)
-
-    assert res.shape == ys.shape
-    assert res.min() >= 0
-    assert res.max() <= 1
 
 
 @pytest.mark.parametrize("holdout_seqs", (["ASDV", None]))
@@ -77,17 +55,3 @@ def test_fit(holdout_seqs, batch_method):
     )
 
     validate_mLSTM_params(params[0], 64)
-
-
-@pytest.mark.skip(reason="Execution test already done in ``test_evotune``.")
-def test_objective():
-    """
-    Placeholder test for the future.
-
-    ``objective()`` gets called as part of ``test_evotune``,
-    and the original idea here was to design an execution-based test
-    for the ``objective()`` function call.
-    Since it's already executed as part of ``test_evotune``,
-    we are skipping this test until we have time to implement it.
-    """
-    assert False
