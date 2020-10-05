d3feb498da1089d0928f025af2b9c975042626eb
Eric Ma <ericmajinglong@gmail.com>
2020-10-05 17:04:26 -0400

    Confirmed that evotuning.py works locally

--- examples/evotuning.py
+++ examples/evotuning.py
@@ -1,7 +1,9 @@
+"""Evotuning two ways."""
 from jax_unirep import evotune, fit
 from jax_unirep.utils import dump_params
+from jax.random import PRNGKey
+from functools import partial
 
-"""Evotuning two ways."""
 
 # Test sequences:
 sequences = ["HASTA", "VISTA", "ALAVA", "LIMED", "HAST", "HAS", "HASVASTA"] * 5
@@ -13,43 +15,27 @@ holdout_sequences = [
     "HAST",
     "HASVALTA",
 ] * 5
+PROJECT_NAME = "evotuning_temp"
+fit_func = partial(
+    fit,
+    mlstm_size=256,
+    rng=PRNGKey(42),
+    epochs_per_print=1,
+)
 
 ## 1. Evotuning with Optuna
-PROJECT_NAME = "temp"
 n_epochs_config = {"low": 1, "high": 1}
 lr_config = {"low": 1e-5, "high": 1e-3}
 study, evotuned_params = evotune(
     sequences=sequences,
-    params=None,
-    proj_name=PROJECT_NAME,
+    fit_func=fit_func,
     out_dom_seqs=holdout_sequences,
     n_trials=2,
     n_splits=2,
     n_epochs_config=n_epochs_config,
     learning_rate_config=lr_config,
-    epochs_per_print=1,
 )
 
 dump_params(evotuned_params, PROJECT_NAME)
 print("Evotuning done! Find output weights in", PROJECT_NAME)
 print(study.trials_dataframe())
-
-
-## 2. Evotuning without Optuna
-
-N_EPOCHS = 3
-LEARN_RATE = 1e-4
-PROJECT_NAME = "temp"
-
-evotuned_params = fit(
-    params=None,
-    sequences=sequences,
-    n_epochs=N_EPOCHS,
-    step_size=LEARN_RATE,
-    holdout_seqs=holdout_sequences,
-    proj_name=PROJECT_NAME,
-    epochs_per_print=1,
-)
-
-dump_params(evotuned_params, PROJECT_NAME)
-print("Evotuning done! Find output weights in", PROJECT_NAME)
