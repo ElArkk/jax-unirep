d2e350832b23b512c820f02e2df3156d3495a4cd
Eric Ma <ericmajinglong@gmail.com>
2020-10-15 18:17:57 -0400

    Update example with newly unified API

--- examples/evotuning.py
+++ examples/evotuning.py
@@ -1,10 +1,11 @@
 """Evotuning two ways!"""
-from functools import partial
 
 from jax.random import PRNGKey
 
-from jax_unirep import evotune, fit
+from jax_unirep import evotune
+from jax_unirep.evotuning_models import mlstm64
 from jax_unirep.utils import dump_params
+from pathlib import Path
 
 
 # Test sequences:
@@ -19,12 +20,19 @@ holdout_sequences = [
 ] * 5
 PROJECT_NAME = "evotuning_temp"
 
+init_fun, apply_fun = mlstm64()
+
+# The input_shape is always going to be (-1, 10),
+# because that is the shape of the input embeddings.
+_, inital_params = init_fun(PRNGKey(42), input_shape=(-1, 10))
 
 # 1. Evotuning with Optuna
 n_epochs_config = {"low": 1, "high": 1}
 lr_config = {"low": 1e-5, "high": 1e-3}
 study, evotuned_params = evotune(
     sequences=sequences,
+    model_func=apply_fun,
+    params=inital_params,
     out_dom_seqs=holdout_sequences,
     n_trials=2,
     n_splits=2,
@@ -32,6 +40,6 @@ study, evotuned_params = evotune(
     learning_rate_config=lr_config,
 )
 
-dump_params(evotuned_params, PROJECT_NAME)
+dump_params(evotuned_params, Path(PROJECT_NAME))
 print("Evotuning done! Find output weights in", PROJECT_NAME)
 print(study.trials_dataframe())
