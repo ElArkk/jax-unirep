47418bade27b7c504f0da3a8058ac576eb1cc493
Eric Ma <ericmajinglong@gmail.com>
2020-12-23 08:28:28 -0500

    Add NaN test for sigmoid and grad of sigmoid

--- jax_unirep/activations.py
+++ jax_unirep/activations.py
@@ -2,14 +2,28 @@ import jax.numpy as np
 
 
 def sigmoid(x, version="tanh"):
-    if version not in ["tanh", "exp"]:
-        raise ValueError("version must be one of ['tanh' or 'exp']")
+    """Sigmoid activation function.
 
+    Two versions are provided: "tanh" and "exp".
+    "exp" is used in the re-implementation.
+    """
     sigmoids = {
         "tanh": lambda x: 0.5 * np.tanh(x) + 0.5,
-        "exp": lambda x: 1 / (1 + np.exp(-x)),
+        "exp": lambda x: safe_sigmoid_exp(x),
     }
     return sigmoids[version](x)
+
+
+def safe_sigmoid_exp(x, clip_value=-88):
+    """
+    Safe version of exp version of sigmoid.
+
+    Based on the test "test_sigmoid", we found that a value of -89.0
+    gives us NaN values when calculating the gradient of sigmoid.
+    As such, we clip -x to a minimum value of -88.0.
+    """
+    x = np.clip(x, a_min=-88)
+    return 1 / (1 + np.exp(-x))
 
 
 def relu(x, alpha=0.1):
