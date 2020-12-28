6b7449417ee59217276bd1826a10a88c8618d996
Eric Ma <ericmjl@users.noreply.github.com>
2020-12-28 14:30:57 -0500

    Update scripts/ci/build_environment.sh
    Co-authored-by: Arkadij Kummer <43340666+ElArkk@users.noreply.github.com>

--- scripts/ci/build_environment.sh
+++ scripts/ci/build_environment.sh
@@ -1,6 +1,6 @@
 conda install -c conda-forge mamba
 conda env create -f environment.yml
-conda activate nams || source activate nams
+conda activate jax-unirep || source activate jax-unirep
 python -m pip install --no-deps .
 conda install conda-pack
 conda pack -n jax-unirep -o jax-unirep.tar.gz
