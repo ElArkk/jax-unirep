15d7ad35db7350ec614464403432f35f67539dc0
Eric Ma <ericmjl@users.noreply.github.com>
2021-01-11 11:14:02 -0500

    [WIP] GitHub actions ci (#97)
    * First commit of new CI system
    
    * Remove Travis builds
    
    * Fix on trigger name
    
    * Add additional comments for documentation
    
    * Add unpack_environment.sh script
    
    * Fix test build step on GitHub actions
    
    * Split out tests into fast and slow
    
    * Debugging fast vs. slow tests
    
    * Fix Makefile syntax bug
    
    * Add code coverage reporting back into pipeline
    
    * Update scripts/ci/build_environment.sh
    
    Co-authored-by: Arkadij Kummer <43340666+ElArkk@users.noreply.github.com>
    
    * Update build_environment.sh
    
    * Leave hints for @ElArkk
    
    * add docs build test on PRs
    
    * deploy docs on gh-pages on push to master
    
    * add make docs
    
    * trying codecov action
    
    * try giving codecov the coverage file directly
    
    * make codecov action less verbose
    
    * leave out codecov action verbose setting
    
    * upload coverage the old way for now
    
    * add build env step
    
    * change docs deploy action
    
    Co-authored-by: Arkadij Kummer <43340666+ElArkk@users.noreply.github.com>
    Co-authored-by: ElArkk <arkadij.kummer@gmail.com>

---
+++ scripts/ci/build_environment.sh
@@ -0,0 +1,6 @@
+conda install -c conda-forge mamba
+mamba env create -f environment.yml
+conda activate jax-unirep || source activate jax-unirep
+python -m pip install --no-deps .
+conda install conda-pack
+conda pack -n jax-unirep -o jax-unirep.tar.gz
