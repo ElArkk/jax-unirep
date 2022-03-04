7c7fa25a6dfe459a13cc8bb99b92fe12d1e325f5
Eric Ma <ericmajinglong@gmail.com>
2022-03-04 07:30:23 -0500

    Update development installation script with instructions.

--- scripts/devinstall-gpu.sh
+++ scripts/devinstall-gpu.sh
@@ -1,3 +1,8 @@
+# Set up a development installation that includes GPU-based JAX install.
+# Run this script by executing the following commands
+# **from the project root directory**:
+#
+#     bash scripts/devinstall-gpu.sh
 mamba env update -f environment.yml
 conda activate jax-unirep
 pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_releases.html  # Note: wheels only available on linux.
